37474f68c5b9a66934b4228734ad56f8d59fb552
Njunu-sk <simonnjunu16@gmail.com>
2019-07-09 14:51:07 -0400

    a forand an array

---
+++ hashandloop.rb
@@ -0,0 +1,13 @@
+family =[
+    "Mike" ,
+    "susan" ,
+    "samuel"
+]
+ for i in family
+    puts "My family: #{i}"
+ end
+for i in family
+    if i == "Mike"
+        puts "He is my small brother"
+    end
+end
