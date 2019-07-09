45b355654bafe13ecbaf2325d4979de33e7b9837
Njunu-sk <simonnjunu16@gmail.com>
2019-07-09 14:30:11 -0400

    some functionality to the folder

---
+++ forloop.rb
@@ -0,0 +1,7 @@
+puts "This program is responsible for printing out number 1-10"
+x = 0
+number = 10
+for x in 0..number
+    puts "This is number #{x}"
+    x =+1
+end
---
+++ untilloop.rb
@@ -0,0 +1,7 @@
+puts "The until loop is similar to the for loop but has a simple syntax"
+x = 0
+number = 10
+until x >= number do
+    puts "hey #{x}"
+    x +=1
+end
