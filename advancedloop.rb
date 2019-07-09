bd0595a26f0c644a6366ab0b56ca2aa117d2d867
Njunu-sk <simonnjunu16@gmail.com>
2019-07-09 15:05:23 -0400

    advance looping using the each in array

---
+++ advancedloop.rb
@@ -0,0 +1,10 @@
+arr = ["simon","paul","mark"]
+for employee in arr
+    puts "Cityright llc employee :#{employee}"
+end
+
+puts "-----------------------------"
+arr.each do |employee|
+    print "Cityright llc employee :#{employee}"
+end
+    
